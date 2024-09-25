665980c5ac91e8635238df466cc5db9eead25526
Ghs-Ash <157726280+Ghs-Ash@users.noreply.github.com>
2024-09-25 21:44:19 +0530

    Update stop_container.sh

--- AWS-codebuild-cicd/stop_container.sh
+++ AWS-codebuild-cicd/stop_container.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-sudo dockerid=`docker ps | awk -F " " 'print $1'`
-sudo docker rm -f $dockerid
+# Capture the first container ID using docker ps and awk
+dockerid=$(sudo docker ps | awk 'NR==2 {print $1}')
+
+# Remove the container if it exists
+if [ -n "$dockerid" ]; then
+  sudo docker rm -f "$dockerid"
+else
+  echo "No running container found."
+fi
